f75fe56366b15432d1a8806ebe9486b9dabb8104
maccam912 <maccam912@gmail.com>
2020-12-20 08:44:35 -0600

    content not solving part b day 19

--- src/day19.rs
+++ src/day19.rs
@@ -106,20 +106,5 @@ mod tests {
 5: \"b\"";
         let regex = day19::parse_rules_to_pest(input2);
         println!("{:?}", regex);
-
-        let input3 = "0: 4 1 5
-1: 2 3 | 3 2
-2: 4 4 | 5 5
-3: 4 5 | 5 4
-4: \"a\"
-5: \"b\"
-
-ababbb
-bababa
-abbbab
-aaabbb
-aaaabbb";
-
-        //assert_eq!(day19::day19(input3, 'a'), 2);
     }
 }
