cf4e9271183169ecedc2e90a03041d22d9118d60
maccam912 <maccam912@gmail.com>
2020-12-23 20:34:32 -0600

    day 23 a

--- src/day23.rs
+++ src/day23.rs
@@ -1,12 +1,17 @@
 use std::collections::VecDeque;
 
-fn setup_game(s: &str) -> (i64, VecDeque<i64>) {
+fn setup_game(s: &str, part: char) -> (i64, VecDeque<i64>) {
     let mut cups = VecDeque::new();
     for c in s.chars() {
         let cupnum = c.to_digit(10).unwrap() as i64;
         cups.push_back(cupnum);
     }
-
+    if part == 'b' {
+        // Add nums 10 through 1000000
+        for n in 10 ..=1000000 {
+            cups.push_back(n);
+        }
+    }
     let current_cup = &cups[0];
     (*current_cup, cups)
 }
@@ -20,17 +25,22 @@ fn get_idx_of(num: i64, v: &VecDeque<i64>) -> i64 {
     -1
 }
 
-fn run_game(_current_cup: i64, cups: &mut VecDeque<i64>) -> String {
+fn run_game(_current_cup: i64, cups: &mut VecDeque<i64>, part: char) -> String {
     let mut current_cup = _current_cup;
-    for i in 0..100 {
-        println!("Move {:?}", i + 1);
+    let numsteps = match part {
+        'a' => 100,
+        'b' => 10000000,
+        _ => panic!("Only parts a and b are allowed."),
+    };
+    for _ in 0..numsteps {
+        //println!("Move {:?}", i + 1);
         let idx = get_idx_of(current_cup, cups);
         cups.rotate_left(idx as usize);
-        println!("cups: {:?}", cups);
+        //println!("cups: {:?}", cups);
         let a = cups.remove(1).unwrap();
         let b = cups.remove(1).unwrap();
         let c = cups.remove(1).unwrap();
-        println!("Pick up: {:?},{:?},{:?}", a, b, c);
+        //println!("Pick up: {:?},{:?},{:?}", a, b, c);
         let mut nextcup = (current_cup - 1).rem_euclid(9);
         if nextcup == 0 {
             nextcup = 9;
@@ -41,7 +51,7 @@ fn run_game(_current_cup: i64, cups: &mut VecDeque<i64>) -> String {
                 nextcup = 9;
             }
         }
-        println!("Destionation: {:?}", nextcup);
+        //println!("Destionation: {:?}", nextcup);
         let next_idx = get_idx_of(nextcup, cups);
         cups.rotate_left(next_idx as usize);
         cups.insert(1, c);
@@ -60,8 +70,13 @@ fn run_game(_current_cup: i64, cups: &mut VecDeque<i64>) -> String {
 }
 
 pub fn day23a(s: &str) -> i64 {
-    let (i, mut cups) = setup_game(s);
-    run_game(i, &mut cups).parse::<i64>().unwrap()
+    let (i, mut cups) = setup_game(s, 'a');
+    run_game(i, &mut cups, 'a').parse::<i64>().unwrap()
+}
+
+pub fn day23b(s: &str) -> i64 {
+    let (i, mut cups) = setup_game(s, 'b');
+    run_game(i, &mut cups, 'b').parse::<i64>().unwrap()
 }
 
 #[cfg(test)]
